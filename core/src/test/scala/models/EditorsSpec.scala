c4bb6fcff8ec13faa482a7831b7b961155a14f80
maroon297 <nirvana297@gmail.com>
2020-05-07 00:29:43 +0900

    Change unit test framework.

--- core/src/test/scala/models/EditorsSpec.scala
+++ core/src/test/scala/models/EditorsSpec.scala
@@ -1,65 +1,61 @@
 package models
 
-import scalikejdbc.specs2.mutable.AutoRollback
-import org.specs2.mutable._
+import org.scalatest._
 import scalikejdbc._
-import java.time.{ZonedDateTime}
+import scalikejdbc.scalatest.AutoRollback
 
 
-class EditorsSpec extends Specification {
+class EditorsSpec extends fixture.FlatSpec with Matchers with AutoRollback {
+  val e = Editors.syntax("e")
 
-  "Editors" should {
+  behavior of "Editors"
 
-    val e = Editors.syntax("e")
-
-    "find by primary keys" in new AutoRollback {
-      val maybeFound = Editors.find("MyString")
-      maybeFound.isDefined should beTrue
-    }
-    "find by where clauses" in new AutoRollback {
-      val maybeFound = Editors.findBy(sqls.eq(e.editorId, "MyString"))
-      maybeFound.isDefined should beTrue
-    }
-    "find all records" in new AutoRollback {
-      val allResults = Editors.findAll()
-      allResults.size should be_>(0)
-    }
-    "count all records" in new AutoRollback {
-      val count = Editors.countAll()
-      count should be_>(0L)
-    }
-    "find all by where clauses" in new AutoRollback {
-      val results = Editors.findAllBy(sqls.eq(e.editorId, "MyString"))
-      results.size should be_>(0)
-    }
-    "count by where clauses" in new AutoRollback {
-      val count = Editors.countBy(sqls.eq(e.editorId, "MyString"))
-      count should be_>(0L)
-    }
-    "create new record" in new AutoRollback {
-      val created = Editors.create(editorId = "MyString", editorName = "MyString", password = "MyString", createTimestamp = null)
-      created should not beNull
-    }
-    "save a record" in new AutoRollback {
-      val entity = Editors.findAll().head
-      // TODO modify something
-      val modified = entity
-      val updated = Editors.save(modified)
-      updated should not equalTo(entity)
-    }
-    "destroy a record" in new AutoRollback {
-      val entity = Editors.findAll().head
-      val deleted = Editors.destroy(entity) == 1
-      deleted should beTrue
-      val shouldBeNone = Editors.find("MyString")
-      shouldBeNone.isDefined should beFalse
-    }
-    "perform batch insert" in new AutoRollback {
-      val entities = Editors.findAll()
-      entities.foreach(e => Editors.destroy(e))
-      val batchInserted = Editors.batchInsert(entities)
-      batchInserted.size should be_>(0)
-    }
+  it should "find by primary keys" in { implicit session =>
+    val maybeFound = Editors.find("MyString")
+    maybeFound.isDefined should be(true)
   }
-
+  it should "find by where clauses" in { implicit session =>
+    val maybeFound = Editors.findBy(sqls.eq(e.editorId, "MyString"))
+    maybeFound.isDefined should be(true)
+  }
+  it should "find all records" in { implicit session =>
+    val allResults = Editors.findAll()
+    allResults.size should be >(0)
+  }
+  it should "count all records" in { implicit session =>
+    val count = Editors.countAll()
+    count should be >(0L)
+  }
+  it should "find all by where clauses" in { implicit session =>
+    val results = Editors.findAllBy(sqls.eq(e.editorId, "MyString"))
+    results.size should be >(0)
+  }
+  it should "count by where clauses" in { implicit session =>
+    val count = Editors.countBy(sqls.eq(e.editorId, "MyString"))
+    count should be >(0L)
+  }
+  it should "create new record" in { implicit session =>
+    val created = Editors.create(editorId = "MyString", editorName = "MyString", password = "MyString", createTimestamp = null)
+    created should not be(null)
+  }
+  it should "save a record" in { implicit session =>
+    val entity = Editors.findAll().head
+    // TODO modify something
+    val modified = entity
+    val updated = Editors.save(modified)
+    updated should not equal(entity)
+  }
+  it should "destroy a record" in { implicit session =>
+    val entity = Editors.findAll().head
+    val deleted = Editors.destroy(entity)
+    deleted should be(1)
+    val shouldBeNone = Editors.find("MyString")
+    shouldBeNone.isDefined should be(false)
+  }
+  it should "perform batch insert" in { implicit session =>
+    val entities = Editors.findAll()
+    entities.foreach(e => Editors.destroy(e))
+    val batchInserted = Editors.batchInsert(entities)
+    batchInserted.size should be >(0)
+  }
 }
