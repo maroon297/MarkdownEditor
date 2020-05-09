2b4211785ec76c22dc86378dedeca35f5615cb2b
maroon297 <nirvana297@gmail.com>
2020-05-09 23:42:37 +0900

    Pass model Editors unit test.

--- core/src/test/scala/models/EditorsSpec.scala
+++ core/src/test/scala/models/EditorsSpec.scala
@@ -1,21 +1,45 @@
 package models
 
+import java.time.ZonedDateTime
+
 import org.scalatest._
 import scalikejdbc._
 import scalikejdbc.scalatest.AutoRollback
-
+import org.scalatest.matchers.should.Matchers
+import scalikejdbc.config.DBs
 
 class EditorsSpec extends fixture.FlatSpec with Matchers with AutoRollback {
+  DBs.setup()
+  override def fixture(implicit session: DBSession): Unit = {
+    SQL("insert into editors values (?,?,?,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP)").bind("testuser","testuser","abc").update().apply()
+    SQL("insert into editors values (?,?,?,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP)").bind("moduser","moduser","def").update().apply()
+    SQL("insert into editors values (?,?,?,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP)").bind("deluser","deluser","ghi").update().apply()
+  }
+
+  override def withFixture(test: OneArgTest): Outcome = {
+    using(db()) { db =>
+      try {
+        db.begin()
+        db.withinTx { implicit session =>
+          fixture(session)
+        }
+        withFixture(test.toNoArgTest(db.withinTxSession()))
+      } finally {
+        db.rollbackIfActive()
+      }
+    }
+  }
+
   val e = Editors.syntax("e")
 
   behavior of "Editors"
 
   it should "find by primary keys" in { implicit session =>
-    val maybeFound = Editors.find("MyString")
+    val maybeFound = Editors.find("testuser")
     maybeFound.isDefined should be(true)
   }
   it should "find by where clauses" in { implicit session =>
-    val maybeFound = Editors.findBy(sqls.eq(e.editorId, "MyString"))
+    val maybeFound = Editors.findBy(sqls.eq(e.editorId, "testuser"))
     maybeFound.isDefined should be(true)
   }
   it should "find all records" in { implicit session =>
@@ -27,21 +51,20 @@ class EditorsSpec extends fixture.FlatSpec with Matchers with AutoRollback {
     count should be >(0L)
   }
   it should "find all by where clauses" in { implicit session =>
-    val results = Editors.findAllBy(sqls.eq(e.editorId, "MyString"))
+    val results = Editors.findAllBy(sqls.eq(e.editorId, "testuser"))
     results.size should be >(0)
   }
   it should "count by where clauses" in { implicit session =>
-    val count = Editors.countBy(sqls.eq(e.editorId, "MyString"))
+    val count = Editors.countBy(sqls.eq(e.editorId, "testuser"))
     count should be >(0L)
   }
   it should "create new record" in { implicit session =>
-    val created = Editors.create(editorId = "MyString", editorName = "MyString", password = "MyString", createTimestamp = null)
+    val created = Editors.create(editorId = "createuser", editorName = "createuser", password = "ghi", createTimestamp = ZonedDateTime.now())
     created should not be(null)
   }
   it should "save a record" in { implicit session =>
     val entity = Editors.findAll().head
-    // TODO modify something
-    val modified = entity
+    val modified = entity.copy(editorId = "modifieduser")
     val updated = Editors.save(modified)
     updated should not equal(entity)
   }
@@ -49,7 +72,7 @@ class EditorsSpec extends fixture.FlatSpec with Matchers with AutoRollback {
     val entity = Editors.findAll().head
     val deleted = Editors.destroy(entity)
     deleted should be(1)
-    val shouldBeNone = Editors.find("MyString")
+    val shouldBeNone = Editors.find("deluser")
     shouldBeNone.isDefined should be(false)
   }
   it should "perform batch insert" in { implicit session =>
